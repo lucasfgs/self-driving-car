ba946401a8847b7c1aa7efd78bfbe85a93e5b89e
Lucas Ferreira <lucsferreira.dev@gmail.com>
2022-06-26 16:25:48 -0400

    feat: add neural network

--- road.js
+++ road.js
@@ -48,7 +48,6 @@ class Road {
     ctx.setLineDash([]);
 
     this.borders.forEach((border) => {
-      console.log(border);
       ctx.beginPath();
       ctx.moveTo(border[0].x, border[0].y);
       ctx.lineTo(border[1].x, border[1].y);
